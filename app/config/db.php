f358c94c5a142d2f7834a8b45534a2899d0fee9a
Elmer <infcle@gmail.com>
2018-03-21 19:30:52 -0400

    configuracon base de datos

--- app/config/db.php
+++ app/config/db.php
@@ -1,13 +1,13 @@
 <?php
-	/*para la base de datos remota*/
-	define('DB_HOST', '208.91.199.124');
+	// para la base de datos remota
+	/*define('DB_HOST', '208.91.199.124');
 	define('DB_USER', 'hakiajfa_central');
 	define('DB_PASS', 'colegiocentral');
-	define('DB_NAME', 'hakiajfa_colegio');
+	define('DB_NAME', 'hakiajfa_colegio');*/
 
-	/*para la base de datos local*/
+	// para la base de datos local
 	define('DB_HOST', 'localhost');
 	define('DB_USER', 'root');
 	define('DB_PASS', '');
-	define('DB_NAME', 'bd_colegio');
+	define('DB_NAME', 'sergui');
 ?>
