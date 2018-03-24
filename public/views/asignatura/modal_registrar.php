cb5e2b91d25372086b741e8cd2b3d46a1a1f3fcb
sergui <juansergio1981@gmail.com>
2018-03-24 17:32:27 -0400

    formulario asignatura

---
+++ public/views/asignatura/modal_registrar.php
@@ -0,0 +1,28 @@
+<div aria-hidden="true" aria-labelledby="myModalLabel" role="dialog" tabindex="-1" id="modal_Registrar" class="modal fade">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button aria-hidden="true" data-dismiss="modal" class="close" type="button">×</button>
+                <h4 class="modal-title">Registrar Nueva Asignatura</h4>
+            </div>
+            <div class="modal-body">
+                <form role="form" id="frmRegistrar" name="frmRegistrar">
+                    <div class="form-group">
+                        <label for="Nombre">Nombre de la asignatura</label>
+                        <input class=" form-control" id="nombre_asignatura" name="nombre_asignatura" type="text" />
+                    </div>
+                        <div class="form-group">
+                        <label for="Nombre">sigla de la asignatura</label>
+                        <input class=" form-control" id="sigla" name="sigla" type="text" />
+                    </div>
+                        
+                    <div class=" modal-footer">
+                        <button type="button" class="btn btn-default" data-dismiss="modal">Cancelar</button>
+                        <button type="submit" class="btn btn-primary" id="btnRegistrar" >Crear</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
