73cdca42dee154bbc8705d2d8661ccc2c38393db
sergui <juansergio1981@gmail.com>
2018-03-24 20:24:33 -0400

    creacion de lista de cursos

---
+++ public/views/curso/index.php
@@ -0,0 +1,169 @@
+<div class="row">
+    <div class="col-sm-12">
+        <section class="panel">
+            <header class="panel-heading">
+                <div class="row panel-heading">
+                    Lista de CURSOS
+                    <span class="pull-right">
+                        <a href="#modal_Registrar" class="btn btn-xs btn-success" data-toggle="modal">
+                            <span class="fa fa-pencil"></span> Nuevo CURSO
+                        </a>
+                    </span>
+                </div>
+            </header>
+            <div class="panel-body">
+                <div class="adv-table" >
+                    <table  class="display table table-bordered table-striped" id="tbSeccion">
+                        <thead>
+                            <tr>
+                                <th>GRADO</th>
+                                <th>PARALELO</th>
+                                <th class="text-center">ACCIONES</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                           
+                                <tr class="gradeX">
+                                    <td><?php echo $curso['id_seccion']; ?></td>
+                                    <td><?php echo $curso['nombre']; ?></td>
+                                    <td >
+                                       <a class="btn btn-success" href="#modalEditar" role="button" data-placement="top" title="Editar" data-toggle="modal" onclick="obtener_datos(<?php echo $curso['id_curso'] ?>)">
+                                        <span class="fa fa-edit" ></span>
+                                    </a>
+                                    <a class="btn btn-danger" href="#modalEliminar" role="button" data-toggle="modal" data-placement="top" title="Eliminar" onclick="eliminar_datos(<?php echo $curso['id_curso'] ?>)">
+                                        <span class="fa fa-trash-o"></span>
+                                    </a>
+                                </td>
+                            </tr>
+                        
+                    </tbody>
+                </table>
+            </div>
+        </div>
+        <?php require_once 'modal_registrar.php'; ?>
+        <?php require_once 'modal_eliminar.php'; ?>
+        <?php require_once 'modal_editar.php'; ?>
+    </section>
+</div>
+</div>
+<script>
+    function obtener_datos(id){
+        $.ajax({
+            url: '../../models/seccion/datos_seccion.php',
+            type: 'POST',
+            dataType: "json",
+            data: {id_seccion: id},
+            success: function(datos){
+                $("#name").val(datos['seccion']['nombre']);
+                $("#id_seccion_modificar").val(datos['seccion']['id_seccion']);
+            }
+        });
+    }
+    function eliminar_datos(id){
+        $("#id_eliminar").val(id);
+    }
+
+    $(document).ready(function() {
+        $('#tbSeccion').dataTable();
+        $("#btnEliminar").click(function(event) {
+            $.ajax({
+                url: '../../models/seccion/eliminar_model.php',
+                type: 'POST',
+                data: $("#frmEliminar").serialize(),
+                beforeSend: function() {
+                    transicion("Procesando Espere....");
+                },
+                success: function(response){
+                    if(response==1){
+                        $('#modalEliminar').modal('hide');
+                        $('#btnEliminar').attr({disabled: 'true'});
+                        transicionSalir();
+                        mensajes_alerta('DATOS ELIMINADOS ELIMINADOS EXITOSAMENTE !! ','success','EDITAR DATOS');
+                        setTimeout(function(){
+                            window.location.href='<?php echo ROOT_CONTROLLER ?>seccion/index.php';
+                        }, 3000);
+                    }else{
+                        transicionSalir();
+                        mensajes_alerta('ERROR AL EDITAR EL USUARIO verifique los datos!! '+response,'error','EDITAR DATOS');
+                    }
+                }
+            });
+        });
+        $('#frmEditar').validate({
+            debug:true,
+            rules:{
+                name:{
+                    required:true,
+                    minlength: 4
+                }
+            },
+            submitHandler: function (form) {
+                $.ajax({
+                    url: '../../models/seccion/editar_model.php',
+                    type: 'post',
+                    data: $("#frmEditar").serialize(),
+                    beforeSend: function() {
+                        transicion("Procesando Espere....");
+                    },
+                    success: function(response) {
+                        if(response==1){
+                            $('#modalEditar').modal('hide');
+                            $('#btnEditar').attr({
+                                disabled: 'true'
+                            });
+                            transicionSalir();
+                            mensajes_alerta('DATOS EDITADOS EXITOSAMENTE !! ','success','EDITAR DATOS');
+                            setTimeout(function(){
+                                window.location.href='<?php echo ROOT_CONTROLLER ?>seccion/index.php';
+                            }, 3000);
+                        }else{
+                            transicionSalir();
+                            mensajes_alerta('ERROR AL EDITAR EL USUARIO verifique los datos!! '+response,'error','EDITAR DATOS');
+                        }
+                    }
+                });
+            }
+        });
+        $("#frmRegistrar").validate({
+            debug:true,
+            rules:{
+                nombre:{
+                    required:true,
+                    minlength: 3,
+                    maxlength:30,
+                }
+            },
+            messages:{
+                nombre:{
+                    required:"Este es Campo Obligatorio.",
+                }
+            },
+            submitHandler: function (form) {
+                $.ajax({
+                    url: '../../models/seccion/registro_model.php',
+                    type: 'post',
+                    data: $("#frmRegistrar").serialize(),
+                    beforeSend: function() {
+                        transicion("Procesando Espere....");
+                    },
+                    success: function(response) {
+                        if(response==1){
+                            $('#btnRegistrar').attr({
+                                disabled: 'true'
+                            });
+                            $('#modal_Registrar').modal('hide');
+                            transicionSalir();
+                            mensajes_alerta('DATOS GUARDADOS EXITOSAMENTE !! ','success','GUARDAR DATOS');
+                            setTimeout(function(){
+                                window.location.href='<?php echo ROOT_CONTROLLER ?>seccion/index.php';
+                            }, 3000);
+                        }else{
+                            transicionSalir();
+                            mensajes_alerta('ERROR AL REGISTRAR ALA SECCION  verifique los datos!! '+response,'error','GUARDAR DATOS');
+                        }
+                    }
+                });
+            }
+        });
+    });
+</script>
---
+++ public/views/curso/modal_registrar.php
@@ -0,0 +1,27 @@
+<div aria-hidden="true" aria-labelledby="myModalLabel" role="dialog" tabindex="-1" id="modal_Registrar" class="modal fade">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button aria-hidden="true" data-dismiss="modal" class="close" type="button">×</button>
+                <h4 class="modal-title">REGISTRO DE NUEVO CURSO</h4>
+            </div>
+            <div class="modal-body">
+                <form role="form" id="frmRegistrar" name="frmRegistrar">
+                    <div class="form-group">
+                        <label for="Nombre">GRADO:</label>
+                        <input class=" form-control" id="grado" name="grado" type="text" />
+                    </div>
+                     <div class="form-group">
+                        <label for="Nombre">PARALELO:</label>
+                        <input class=" form-control" id="paralelo" name="paralelo" type="text" />
+                    </div>
+                    <div class=" modal-footer">
+                        <button type="button" class="btn btn-default" data-dismiss="modal">Cancelar</button>
+                        <button type="submit" class="btn btn-primary" id="btnRegistrar" >Crear</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
