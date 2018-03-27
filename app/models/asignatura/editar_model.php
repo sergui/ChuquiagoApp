81240f700328dba5ea3667df4038875dd27c445e
laiwett <l.mendoza.liet@gmail.com>
2018-03-27 12:00:11 -0400

    edit

--- app/models/asignatura/editar_model.php
+++ app/models/asignatura/editar_model.php
@@ -3,6 +3,7 @@
 	require_once ("../../config/conexion.php");
 
 	//echo "<pre>";print_r ($_REQUEST);echo "</pre>";
+	$id                = trim($_POST["id_asignatura"]);
 	$nombre_asignatura = trim($_POST["nombre_asignatura"]);
 	$sigla             = trim($_POST["sigla"]);
 	
