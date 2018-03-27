319cf786b43306d595780d642e2b0b252ccbfffd
sergui <juansergio1981@gmail.com>
2018-03-27 14:00:41 -0400

    abm asignatura

--- public/views/asignatura/index.php
+++ public/views/asignatura/index.php
@@ -48,100 +48,66 @@
 </div>
 </div>
 <script>
-    function obtener_datos(id){
-        $.ajax({
-            url: '../../models/seccion/datos_seccion.php',
+    
+     function obtener_datos(id)
+         {
+            
+            $.ajax({
+            url: '../../models/asignatura/datos_asignatura.php',
             type: 'POST',
             dataType: "json",
-            data: {id_seccion: id},
-            success: function(datos){
-                $("#name").val(datos['seccion']['nombre']);
-                $("#id_seccion_modificar").val(datos['seccion']['id_seccion']);
-            }
-        });
-    }
-    function eliminar_datos(id){
-        $("#id_eliminar").val(id);
-    }
+            data: {id_asignatura: id},
+            success: function(datos){ 
+               
+                $("#frmEditar [id=nombre_asignatura]").val(datos['asignatura']['nombre_asignatura']);
+                
+                $("#frmEditar [id=sigla]").val(datos['asignatura']['sigla']);
 
-    $(document).ready(function() {
-        $('#tbAsignatura').dataTable();
-        $("#btnEliminar").click(function(event) {
-            $.ajax({
-                url: '../../models/seccion/eliminar_model.php',
-                type: 'POST',
-                data: $("#frmEliminar").serialize(),
-                beforeSend: function() {
-                    transicion("Procesando Espere....");
-                },
-                success: function(response){
-                    if(response==1){
-                        $('#modalEliminar').modal('hide');
-                        $('#btnEliminar').attr({disabled: 'true'});
-                        transicionSalir();
-                        mensajes_alerta('DATOS ELIMINADOS ELIMINADOS EXITOSAMENTE !! ','success','EDITAR DATOS');
-                        setTimeout(function(){
-                            window.location.href='<?php echo ROOT_CONTROLLER ?>seccion/index.php';
-                        }, 3000);
-                    }else{
-                        transicionSalir();
-                        mensajes_alerta('ERROR AL EDITAR EL USUARIO verifique los datos!! '+response,'error','EDITAR DATOS');
-                    }
-                }
-            });
-        });
-        $('#frmEditar').validate({
-            debug:true,
-            rules:{
-                name:{
-                    required:true,
-                    minlength: 4
-                }
-            },
-            submitHandler: function (form) {
-                $.ajax({
-                    url: '../../models/seccion/editar_model.php',
-                    type: 'post',
-                    data: $("#frmEditar").serialize(),
-                    beforeSend: function() {
-                        transicion("Procesando Espere....");
-                    },
-                    success: function(response) {
-                        if(response==1){
-                            $('#modalEditar').modal('hide');
-                            $('#btnEditar').attr({
-                                disabled: 'true'
-                            });
-                            transicionSalir();
-                            mensajes_alerta('DATOS EDITADOS EXITOSAMENTE !! ','success','EDITAR DATOS');
-                            setTimeout(function(){
-                                window.location.href='<?php echo ROOT_CONTROLLER ?>seccion/index.php';
-                            }, 3000);
-                        }else{
-                            transicionSalir();
-                            mensajes_alerta('ERROR AL EDITAR EL USUARIO verifique los datos!! '+response,'error','EDITAR DATOS');
-                        }
-                    }
-                });
+                $("#id_asignatura").val(datos['asignatura']['id_asignatura']);//enviando id para el modelo
+
+                
+                
+                
             }
-        });
+            });
+        }
+
+    ///////////////////ELIMINAR DATOS////////
+    function eliminar_datos(id)
+    {
+        $("#id_eliminar").val(id);
+    }    
+    ////////////////////JQUERY/////////////////////
+    $(document).ready(function() 
+    {
+         
+        /////////////REGISTRAR DATOS////////////////
         $("#frmRegistrar").validate({
             debug:true,
-            rules:{
-                nombre:{
+            rules:
+            {
+                nombre_asignatura:{
                     required:true,
                     minlength: 3,
-                    maxlength:30,
+                    maxlength:15,
+                },
+                sigla:{
+                    required:true,
+                    minlength: 1,
+                    maxlength:5,
                 }
             },
             messages:{
-                nombre:{
-                    required:"Este es Campo Obligatorio.",
-                }
+               nombre_asignatura:{
+                    required:"Este es Campo Obligatorioooo.",
+                },
+                sigla:{
+                    required:"Este es Campo Obligatorioooo.",
+                },
             },
             submitHandler: function (form) {
                 $.ajax({
-                    url: '../../models/seccion/registro_model.php',
+                    url: '../../models/asignatura/registro_model.php',
                     type: 'post',
                     data: $("#frmRegistrar").serialize(),
                     beforeSend: function() {
@@ -156,7 +122,7 @@
                             transicionSalir();
                             mensajes_alerta('DATOS GUARDADOS EXITOSAMENTE !! ','success','GUARDAR DATOS');
                             setTimeout(function(){
-                                window.location.href='<?php echo ROOT_CONTROLLER ?>seccion/index.php';
+                                window.location.href='<?php echo ROOT_CONTROLLER ?>asignatura/index.php';
                             }, 3000);
                         }else{
                             transicionSalir();
@@ -166,5 +132,82 @@
                 });
             }
         });
+        /////////////editar DATOS////////////////
+        $('#frmEditar').validate({
+            debug:true,
+            rules:
+            {
+                nombre_asignatura:{
+                    required:true,
+                    minlength: 3,
+                    maxlength:15,
+                },
+                sigla:{
+                    required:true,
+                    minlength: 1,
+                    maxlength:5,
+                }
+            },
+            messages:{
+               nombre_asignatura:{
+                    required:"Este es Campo Obligatorioooo.",
+                },
+                sigla:{
+                    required:"Este es Campo Obligatorioooo.",
+                },
+            },
+            submitHandler: function (form) {
+                $.ajax({
+                    url: '../../models/asignatura/editar_model.php',
+                    type: 'post',
+                    data: $("#frmEditar").serialize(),
+                    beforeSend: function() {
+                        transicion("Procesando Espere....");
+                    },
+                    success: function(response) {
+                        if(response==1){
+                            $('#modalEditar').modal('hide');
+                            $('#btnEditar').attr({
+                                disabled: 'true'
+                            });
+                            transicionSalir();
+                            mensajes_alerta('DATOS EDITADOS EXITOSAMENTE !! ','success','EDITAR DATOS');
+                            setTimeout(function(){
+                                window.location.href='<?php echo ROOT_CONTROLLER ?>asignatura/index.php';
+                            }, 3000);
+                        }else{
+                            transicionSalir();
+                            mensajes_alerta('ERROR AL EDITAR EL USUARIO verifique los datos!! '+response,'error','EDITAR DATOS');
+                        }
+                    }
+                });
+            }
+        });
+        /////////////ELIMINAR DATOS////////////////
+         $("#btnEliminar").click(function(event) {
+            $.ajax({
+                url: '../../models/asignatura/eliminar_model.php',
+                type: 'POST',
+                data: $("#frmEliminar").serialize(),
+                beforeSend: function() {
+                    transicion("Procesando Espere....");
+                },
+                success: function(response){
+                    if(response==1){
+                        $('#modalEliminar').modal('hide');
+                        $('#btnEliminar').attr({disabled: 'true'});
+                        transicionSalir();
+                        mensajes_alerta('DATOS ELIMINADOS ELIMINADOS EXITOSAMENTE !! ','success','EDITAR DATOS');
+                        setTimeout(function(){
+                            window.location.href='<?php echo ROOT_CONTROLLER ?>asignatura/index.php';
+                        }, 3000);
+                    }else{
+                        transicionSalir();
+                        mensajes_alerta('ERROR AL EDITAR EL USUARIO verifique los datos!! '+response,'error','EDITAR DATOS');
+                    }
+                }
+            });
+        });
+     
     });
 </script>
