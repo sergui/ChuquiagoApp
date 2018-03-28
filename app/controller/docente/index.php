a9e556dd18a580ac96b1988a85c1fa871a38bdf7
Jane <jeaneinfo@gmail.com>
2018-03-27 20:25:46 -0400

    controladores menu

--- app/controller/docente/index.php
+++ app/controller/docente/index.php
@@ -8,14 +8,14 @@
 	require_once ("../../config/db.php");
 	require_once ("../../config/conexion.php");
 	//Variables para enviar a la plantilla
-	$titulo="Lista de docentes";
-	$contenido="user/listar_docente.php";
-	$subTitulo="Lista de docentes";
+	$titulo="Docentes";
+	$contenido="docente/index.php";
+	$subTitulo="Docente";
 	$menu_a= $menus['U_LISTA'];
 
-	/*if (!($usuarios = $con->query("SELECT * FROM usuario_login where estado = 1 and id_usuario<>{$_SESSION['id_user']} and tipo<>0"))) {
+	if (!($usuarios = $con->query("SELECT * FROM usuario where estado = 1 "))) {
     	echo "Falló SELECT: (" . $con->errno . ") " . $con->error;
-	}*/
+	}
 	//var_dump($usuarios->fetch_assoc());
 	$pie_class="si";//Variable donde se poneun pie de pagina estatico	
 	require_once ('../../../public/views/plantilla.php');
