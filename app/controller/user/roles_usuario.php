fdb32f4c9b3ca14d98f06120dd0783fd54072646
Jane <jeaneinfo@gmail.com>
2018-03-28 12:35:57 -0400

    Asgnando vistas para los roles

--- app/controller/user/roles_usuario.php
+++ app/controller/user/roles_usuario.php
@@ -1,10 +1,10 @@
 <?php
 	require_once '../../config/route.php';
-	session_start();
+	/*session_start();
 	if (!isset($_SESSION['<us></us>er_login_status']) AND $_SESSION['user_login_status'] != 1) {
         header("location: ".ROOT_CONTROLLER.'login/');
 		exit;
-	}
+	}*/
 	require_once ("../../config/db.php");
 	require_once ("../../config/conexion.php");
 	//Variables para enviar a la plantilla
@@ -16,7 +16,7 @@
 	if (!($usuarios = $con->query("SELECT * FROM roles where estado = 1 "))) {
     	echo "Falló SELECT: (" . $con->errno . ") " . $con->error;
 	}
-	//var_dump($usuarios->fetch_assoc());
+
 	$pie_class="si";//Variable donde se poneun pie de pagina estatico	
 	require_once ('../../../public/views/plantilla.php');
 ?>
---
+++ public/views/user/roles_usuario.php
@@ -0,0 +1,1 @@
+aqui realizar los roles
