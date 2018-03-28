87f7d1930c0f2d9e8215e0f6291bf22277f6bc07
sergui <juansergio1981@gmail.com>
2018-03-27 22:04:13 -0400

    ABM MODELO CITACION

---
+++ public/views/citacion/modal_registrar.php
@@ -0,0 +1,24 @@
+<div aria-hidden="true" aria-labelledby="myModalLabel" role="dialog" tabindex="-1" id="modal_Registrar" class="modal fade">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button aria-hidden="true" data-dismiss="modal" class="close" type="button">×</button>
+                <h4 class="modal-title">REGISTRO DE NUEVO MODELO DE CITACION</h4>
+            </div>
+            <div class="modal-body">
+                <form role="form" id="frmRegistrar" name="frmRegistrar">
+                    <div class="form-group">
+                        <label for="Nombre">ESCRIBA LA CITACION:</label>
+                        <input class=" form-control" id="formato" name="formato" type="text" />
+                    </div>
+                 
+                    <div class=" modal-footer">
+                        <button type="button" class="btn btn-default" data-dismiss="modal">Cancelar</button>
+                        <button type="submit" class="btn btn-primary" id="btnRegistrar" >Crear</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
