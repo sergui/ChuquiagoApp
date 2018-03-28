f84bb4f2dec1d4fa62a964e5e0da85882768e68a
Jane <jeaneinfo@gmail.com>
2018-03-28 17:03:35 -0400

    aumentando controlador y cista padres y estudiante

--- app/controller/padres/index.php
+++ app/controller/padres/index.php
@@ -1,19 +1,19 @@
 <?php
 	require_once '../../config/route.php';
-	session_start();
+	/*session_start();
 	if (!isset($_SESSION['<us></us>er_login_status']) AND $_SESSION['user_login_status'] != 1) {
         header("location: ".ROOT_CONTROLLER.'login/');
 		exit;
-	}
+	}*/
 	require_once ("../../config/db.php");
 	require_once ("../../config/conexion.php");
 	//Variables para enviar a la plantilla
 	$titulo="Padres de familia";
-	$contenido="padres/index.php";
+	$contenido="tutor/index.php";
 	$subTitulo="Padre de familia";
 	$menu_a= $menus['U_LISTA'];
 
-	if (!($usuarios = $con->query("SELECT * FROM usuario where estado = 1 "))) {
+	if (!($usuarios = $con->query("SELECT * FROM tutor where estado = 1 "))) {
     	echo "Falló SELECT: (" . $con->errno . ") " . $con->error;
 	}
 	//var_dump($usuarios->fetch_assoc());
