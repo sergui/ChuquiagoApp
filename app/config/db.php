23fc0abc523ed584e8ec84f7befb193ab8940b92
Elmer Coronel <infcle@gmail.com>
2018-03-29 16:02:30 -0400

    cambiando base de datos de desarrollo

--- app/config/db.php
+++ app/config/db.php
@@ -6,8 +6,14 @@
 	define('DB_NAME', 'hakiajfa_colegio');*/
 
 	// para la base de datos local
-	define('DB_HOST', 'localhost');
+	/*define('DB_HOST', 'localhost');
 	define('DB_USER', 'root');
 	define('DB_PASS', '');
+	define('DB_NAME', 'bdchuquiago');*/
+	
+	// para la base de datos remota de desarrollo
+	define('DB_HOST', '192.168.1.8');
+	define('DB_USER', 'hakiari');
+	define('DB_PASS', 'vVKcJwqGHnMIVTCI');
 	define('DB_NAME', 'bdchuquiago');
 ?>
