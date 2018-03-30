c4f1ecae187f037cc340a85a8069dbeb5b18ab02
sergui <juansergio1981@gmail.com>
2018-03-30 00:07:00 -0400

    abm estudiante

--- public/views/estudiante/index.php
+++ public/views/estudiante/index.php
@@ -1,1 +1,252 @@
-aqui modelo de estudiante
+<div class="row">
+    <div class="col-sm-12">
+        <section class="panel">
+            <header class="panel-heading">
+                <div class="row panel-heading">
+                    Lista de estudiantes
+                    <span class="pull-right">
+                        <a href="#modal_Registrar" class="btn btn-xs btn-success" data-toggle="modal">
+                            <span class="fa fa-pencil"></span> REGISTRAR ESTUDIANTE
+                        </a>
+                    </span>
+                </div>
+            </header>
+            <div class="panel-body">
+                <div class="adv-table" >
+                    <table  class="display table table-bordered table-striped" id="tbEstudiante"">
+                        <thead>
+                            <tr>
+                                <th>NOMBRES </th>
+                                <th>APELLIDO PATERNO</th>
+                                <th class="text-center">Acciones</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php foreach ($estudiantes as $estudiante): ?>
+                                <tr class="gradeX">
+                                    <td><?php echo $estudiante['nombre']; ?></td>
+                                    <td><?php echo $estudiante['paterno']; ?></td>
+                                    <td >
+                                       <a class="btn btn-success" href="#modalEditar" role="button" data-placement="top" title="Editar" data-toggle="modal" onclick="<obtener_datos(<?php echo $estudiante['id_rude'] ?>)">
+                                        <span class="fa fa-edit" ></span>
+                                    </a>
+                                    <a class="btn btn-danger" href="#modalEliminar" role="button" data-toggle="modal" data-placement="top" title="Eliminar" onclick="eliminar_datos(<?php echo $estudiante['id_rude'] ?>)">
+                                        <span class="fa fa-trash-o"></span>
+                                    </a>
+                                </td>
+                            </tr>
+                        <?php endforeach;?>
+
+                    </tbody>
+                </table>
+            </div>
+        </div>
+        <?php require_once 'modal_registrar.php'; ?>
+       <!--  <?php require_once 'modal_eliminar.php'; ?>
+        <?php require_once 'modal_editar.php'; ?> -->
+    </section>
+</div>
+</div>
+<script>
+    
+     // function obtener_datos(id)
+     //     {
+            
+     //        $.ajax({
+     //            url: '../../models/asignatura/datos_asignatura.php',
+     //            type: 'POST',
+     //            dataType: "json",
+     //            data: {id_asignatura: id},
+     //            success: function(datos){ 
+                   
+     //                $("#frmEditar [id=nombre_asignatura]").val(datos['asignatura']['nombre_asignatura']);
+                    
+     //                $("#frmEditar [id=sigla]").val(datos['asignatura']['sigla']);
+
+     //                $("#id_asignatura").val(datos['asignatura']['id_asignatura']);//enviando id para el modelo
+     //            }
+     //        });
+     //    }
+
+    ///////////////////ELIMINAR DATOS////////
+  /*  function eliminar_datos(id)
+    {
+        $("#id_eliminar").val(id);
+    } */   
+    ////////////////////JQUERY/////////////////////
+    $(document).ready(function() 
+    {
+    	/////////////////PLUGINS FECHA///////////////
+    	$('.cFecha').datepicker({
+			format: 'dd/mm/yyyy'
+		})
+		.on('changeDate', function(ev){
+			$('.cFecha').datepicker('hide');
+		});
+
+		////////////////busquea de datos/////////////////
+
+        $("#tbEstudiante").dataTable();
+        /////////////REGISTRAR DATOS////////////////
+        $("#frmRegistrar").validate({
+            debug:true,
+            rules:
+            {
+                nombre:{
+                    required:true,
+                    minlength: 3,
+                    maxlength:15,
+                },
+                paterno:{
+                    required:true,
+                    minlength: 1,
+                    maxlength:15,
+                },
+
+                materno:{
+                    required:true,
+                    minlength: 1,
+                    maxlength:15,
+                },
+
+                domicilio:{
+                    required:true,
+                    minlength: 1,
+                    maxlength:200,
+                },
+                sexo:{
+                    required:true,
+                    minlength: 1,
+                    maxlength:5,
+                },
+                fecha_nac:{
+                    required:true,
+                    
+                }
+            },
+            messages:{
+		                nombre:{
+		                    required:"Este es Campo Obligatorioooo.",
+		                },
+		                paterno:{
+		                    required:"Este es Campo Obligatorioooo.",
+		                },
+		                 materno:{
+		                    required:"Este es Campo Obligatorioooo.",
+		                },
+		                domicilio:{
+		                    required:"Este es Campo Obligatorioooo.",
+		                },
+		                 sexo:{
+		                    required:"Este es Campo Obligatorioooo.",
+		                },
+		                fecha_nac:{
+		                    required:"Este es Campo Obligatorioooo.",
+		                },
+            },
+            submitHandler: function (form) {
+                $.ajax({
+                    url: '../../models/estudiante/registro_model.php',
+                    type: 'post',
+                    data: $("#frmRegistrar").serialize(),
+                    beforeSend: function() {
+                        transicion("Procesando Espere....");
+                    },
+                    success: function(response) {
+                        if(response==1){
+                            $('#btnRegistrar').attr({
+                                disabled: 'true'
+                            });
+                            $('#modal_Registrar').modal('hide');
+                            transicionSalir();
+                            mensajes_alerta('DATOS GUARDADOS EXITOSAMENTE !! ','success','GUARDAR DATOS');
+                            setTimeout(function(){
+                                window.location.href='<?php echo ROOT_CONTROLLER ?>estudiante/index.php';
+                            }, 3000);
+                        }else{
+                            transicionSalir();
+                            mensajes_alerta('ERROR AL REGISTRAR AL ESTUDIANTE  verifique los datos!! '+response,'error','GUARDAR DATOS');
+                        }
+                    }
+                });
+            }
+        });
+        /////////////editar DATOS////////////////
+        /*$('#frmEditar').validate({
+            debug:true,
+            rules:
+            {
+                nombre_asignatura:{
+                    required:true,
+                    minlength: 3,
+                    maxlength:15,
+                },
+                sigla:{
+                    required:true,
+                    minlength: 1,
+                    maxlength:5,
+                }
+            },
+            messages:{
+               nombre_asignatura:{
+                    required:"Este es Campo Obligatorioooo.",
+                },
+                sigla:{
+                    required:"Este es Campo Obligatorioooo.",
+                },
+            },
+            submitHandler: function (form) {
+                $.ajax({
+                    url: '../../models/asignatura/editar_model.php',
+                    type: 'post',
+                    data: $("#frmEditar").serialize(),
+                    beforeSend: function() {
+                        transicion("Procesando Espere....");
+                    },
+                    success: function(response) {
+                        if(response==1){
+                            $('#modalEditar').modal('hide');
+                            $('#btnEditar').attr({
+                                disabled: 'true'
+                            });
+                            transicionSalir();
+                            mensajes_alerta('DATOS EDITADOS EXITOSAMENTE !! ','success','EDITAR DATOS');
+                            setTimeout(function(){
+                                window.location.href='<?php echo ROOT_CONTROLLER ?>asignatura/index.php';
+                            }, 3000);
+                        }else{
+                            transicionSalir();
+                            mensajes_alerta('ERROR AL EDITAR EL USUARIO verifique los datos!! '+response,'error','EDITAR DATOS');
+                        }
+                    }
+                });
+            }
+        });*/
+        /////////////ELIMINAR DATOS////////////////
+        /* $("#btnEliminar").click(function(event) {
+            $.ajax({
+                url: '../../models/asignatura/eliminar_model.php',
+                type: 'POST',
+                data: $("#frmEliminar").serialize(),
+                beforeSend: function() {
+                    transicion("Procesando Espere....");
+                },
+                success: function(response){
+                    if(response==1){
+                        $('#modalEliminar').modal('hide');
+                        $('#btnEliminar').attr({disabled: 'true'});
+                        transicionSalir();
+                        mensajes_alerta('DATOS ELIMINADOS ELIMINADOS EXITOSAMENTE !! ','success','EDITAR DATOS');
+                        setTimeout(function(){
+                            window.location.href='<?php echo ROOT_CONTROLLER ?>asignatura/index.php';
+                        }, 3000);
+                    }else{
+                        transicionSalir();
+                        mensajes_alerta('ERROR AL EDITAR EL USUARIO verifique los datos!! '+response,'error','EDITAR DATOS');
+                    }
+                }
+            });*/
+        });
+     
+    });
+</script>
---
+++ public/views/estudiante/modal_registrar.php
@@ -0,0 +1,66 @@
+<div aria-hidden="true" aria-labelledby="myModalLabel" role="dialog" tabindex="-1" id="modal_Registrar" class="modal fade">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button aria-hidden="true" data-dismiss="modal" class="close" type="button">×</button>
+                <h4 class="modal-title">REGISTRO DE NUEVO ESTUDIANTE</h4>
+            </div>
+            <div class="modal-body">
+                <form role="form" id="frmRegistrar" name="frmRegistrar">
+                    
+                    <div class="form-group">
+                        <label for="Nombre">Nombre(s)</label>
+                        <input class=" form-control" id="nombre" name="nombre" type="text" />
+                    </div>
+                    
+                    <div class="form-group">
+                        <label for="Nombre">Apellido Paterno</label>
+                        <input class=" form-control" id="paterno" name="paterno" type="text" />
+                    </div>
+
+                    <div class="form-group">
+                        <label for="Nombre">Apellido Materno</label>
+                        <input class=" form-control" id="materno" name="materno" type="text" />
+                    </div>
+
+                     <div class="form-group">
+                        <label class="control-label">Fecha  de nacimiento:</label>
+                        <div data-date-viewmode="date" data-date="<?php echo date('d/m/Y'); ?>"  class="input-append date cFecha">
+                            <input type="text" readonly="" value="" size="16" class="form-control" name="fecha_nac" id="fecha_nac">
+                            <span class="input-group-lg add-on">
+                                <button class="btn btn-primary" type="button"><i class="fa fa-calendar"></i></button>
+                            </span>
+                        </div>
+                    </div>
+
+                    
+                     <div class="form-group">
+                        <label for="Nombre">Sexo</label>
+                        <div class="radio">
+                        <label><input type="radio" name="sexo" id="sexom" value="m">Masculino</label><br>
+                        <label><input type="radio" name="sexo" id="sexof" value="f">Femenino</label>
+                        </div>
+                    </div>
+
+                     <div class="form-group">
+                        <label for="Nombre">Domicilio</label>
+                        <input class=" form-control" id="dimicilio" name="domicilio" type="text" />
+                    </div>
+                   
+
+
+                 
+<!-- 
+                           -->
+                  
+                        
+                    <div class=" modal-footer">
+                        <button type="button" class="btn btn-default" data-dismiss="modal">Cancelar</button>
+                        <button type="submit" class="btn btn-primary" id="btnRegistrar" >Crear</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
