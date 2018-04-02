0d4c33f99c1c040e7acd2b452059ae875a2948be
sergui <juansergio1981@gmail.com>
2018-04-02 16:04:50 -0400

    abm docente

--- public/views/docente/modal_editar.php
+++ public/views/docente/modal_editar.php
@@ -3,20 +3,29 @@
         <div class="modal-content">
             <div class="modal-header">
                 <button aria-hidden="true" data-dismiss="modal" class="close" type="button">×</button>
-                <h4 class="modal-title">EDITAR CURSO</h4>
+                <h4 class="modal-title">EDITAR DATOS DEL DOCENTE</h4>
             </div>
             <div class="modal-body">
                 <form role="form" id="frmEditar" name="frmEditar">
-                    <input type="hidden" name="id_curso" id="id_curso" class="form-control" value="">
+                    <input type="hidden" name="id_docente" id="id_docente" class="form-control" value="">
                     <div class="form-group">
-                        <label for="Nombre">NUEVO NOMBRE DEL CURSO</label>
-                        <input class=" form-control" id="grado" name="grado" minlength="7 " type="text" required autofocus="true" />
-                    </div><br>
+                        <label for="Nombre">Editar nombre</label>
+                        <input class=" form-control" id="nombre" name="nombre" minlength="7 " type="text" required autofocus="true" />
+                    </div>
+                      <div class="form-group">
+                        <label for="Nombre">Editar apellido paterno</label>
+                        <input class=" form-control" id="paterno" name="paterno" minlength="7 " type="text" required autofocus="true" />
+                    </div>
+                      <div class="form-group">
+                        <label for="Nombre">Editar apellido materno</label>
+                        <input class=" form-control" id="materno" name="materno" minlength="7 " type="text" required autofocus="true" />
+                    </div>
+                      <div class="form-group">
+                        <label for="Nombre">Editar celular</label>
+                        <input class=" form-control" id="celular" name="celular" minlength="7 " type="text" required autofocus="true" />
+                    </div>
+                 
 
-                     <div class="form-group">
-                        <label for="Nombre">NUEVO NOMBRE DEL PARALELO</label>
-                        <input class=" form-control" id="paralelo" name="paralelo" minlength="7 " type="text" required autofocus="true" />
-                    </div><br>
                  
                     <div class=" modal-footer">
                         <button type="button" class="btn btn-default" data-dismiss="modal">Cancelar</button>
