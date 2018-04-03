325ed269fcbf8f0a0f3a7ea642d4e388f37e322b
Jane <jeaneinfo@gmail.com>
2018-04-03 12:08:36 -0400

    arreglando sesion

--- app/controller/login/index.php
+++ app/controller/login/index.php
@@ -2,11 +2,6 @@
 	require_once '../../config/db.php';
 	require_once '../../config/route.php';
 	require_once '../../models/login/Login.php';
-	session_start();
-	if (!isset($_SESSION['user_login_status']) AND $_SESSION['user_login_status'] != 1) {
-        header("location: ".ROOT_CONTROLLER.'login/');
-		exit;
-	}
 
 	$mlogin = new Login();
 	//Variables para enviar a la plantilla
