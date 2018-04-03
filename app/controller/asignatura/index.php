5266baa0017112e762cb2c4776921ef81263c0af
Jane <jeaneinfo@gmail.com>
2018-04-03 12:03:42 -0400

    habilitando sesiones

--- app/controller/asignatura/index.php
+++ app/controller/asignatura/index.php
@@ -1,11 +1,11 @@
 
 <?php
 	require_once '../../config/route.php';
-	/*session_start();
+	session_start();
 	if (!isset($_SESSION['user_login_status']) AND $_SESSION['user_login_status'] != 1) {
         header("location: ".ROOT_CONTROLLER.'login/');
 		exit;
-	}*/
+	}
 	require_once ("../../config/db.php");
 	require_once ("../../config/conexion.php");
 	//Variables para enviar a la plantilla
@@ -13,7 +13,7 @@
 	$contenido="asignatura/index.php";
 	$menu_a= $menus['C_SECCION'];
 	$subTitulo="ASIGNATURA";
-	
+
 	if (!($asignaturas = $con->query("SELECT * FROM ASIGNATURA where estado=1"))) {
     	echo "Falló SELECT: (" . $con->errno . ") " . $con->error;
 	}
--- app/controller/kardex/index.php
+++ app/controller/kardex/index.php
@@ -1,10 +1,10 @@
 <?php
 	require_once '../../config/route.php';
-	/*session_start();
+	session_start();
 	if (!isset($_SESSION['user_login_status']) AND $_SESSION['user_login_status'] != 1) {
         header("location: ".ROOT_CONTROLLER.'login/');
 		exit;
-	}*/
+	}
 	require_once ("../../config/db.php");
 	require_once ("../../config/conexion.php");
 	//Variables para enviar a la plantilla
