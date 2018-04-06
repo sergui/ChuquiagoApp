af0b4a8e9bbf9c61c362832e1a3c22be8508fd8c
laiwett <l.mendoza.liet@gmail.com>
2018-04-06 11:22:47 -0400

    bd

---
+++ Script bd/bdchuquiagoVersion E.sql
@@ -0,0 +1,592 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.5.2
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 06-04-2018 a las 17:11:29
+-- Versión del servidor: 10.1.21-MariaDB
+-- Versión de PHP: 5.6.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `bdchuquiago`
+--
+DROP DATABASE IF EXISTS `bdchuquiago`;
+CREATE DATABASE IF NOT EXISTS `bdchuquiago` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `bdchuquiago`;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `asignatura`
+--
+
+DROP TABLE IF EXISTS `asignatura`;
+CREATE TABLE `asignatura` (
+  `id_asignatura` bigint(11) NOT NULL,
+  `nombre_asignatura` varchar(15) CHARACTER SET utf8 COLLATE utf8_spanish2_ci NOT NULL,
+  `sigla` varchar(7) CHARACTER SET utf8 COLLATE utf8_spanish2_ci NOT NULL,
+  `estado` bigint(1) NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `asignatura`
+--
+
+INSERT INTO `asignatura` (`id_asignatura`, `nombre_asignatura`, `sigla`, `estado`) VALUES
+(1, 'MATEMATICAS', 'MAT', 1),
+(2, 'QUIMICA', 'QMC', 1),
+(3, 'BIOLOGIA', 'BIO', 1),
+(4, 'PSICOLOGIA', 'PSC', 1),
+(5, 'FISICA', 'FIS', 1),
+(6, 'LITERATURA', 'LIT', 1),
+(7, 'ARTES PLASTICAS', 'ART', 1),
+(8, 'MUSICA', 'MUS', 1),
+(9, 'FILISOFIA', 'FIL', 1),
+(10, 'EDUCACION CIVIC', 'CIV', 1),
+(11, 'HISTORIA', 'HIS', 1),
+(12, 'CIENCIAS NATURA', 'CNT', 1),
+(13, 'GEOGRAFIA', 'GEO', 1),
+(14, 'TECNICA VOCACIO', 'TEC', 1),
+(15, 'EDUCACION FISIC', 'EDU', 1),
+(16, 'IDIOMAS', 'IDI', 1),
+(17, 'RELIGION', 'REL', 1),
+(18, 'CIENCIAS SOCIAL', 'CSO', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `citacion`
+--
+
+DROP TABLE IF EXISTS `citacion`;
+CREATE TABLE `citacion` (
+  `id_citacion` int(11) NOT NULL,
+  `tipo` varchar(10) CHARACTER SET utf8 COLLATE utf8_spanish2_ci NOT NULL,
+  `citacion` varchar(20) CHARACTER SET utf8 COLLATE utf8_spanish2_ci NOT NULL,
+  `fecha` datetime NOT NULL,
+  `estado` tinyint(1) NOT NULL DEFAULT '1',
+  `id_kardex` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `curso`
+--
+
+DROP TABLE IF EXISTS `curso`;
+CREATE TABLE `curso` (
+  `id_curso` int(11) NOT NULL,
+  `grado` varchar(20) NOT NULL,
+  `paralelo` varchar(8) NOT NULL,
+  `estado` tinyint(1) NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `curso`
+--
+
+INSERT INTO `curso` (`id_curso`, `grado`, `paralelo`, `estado`) VALUES
+(1, '1RO', 'A', 1),
+(2, '1RO', 'B', 1),
+(3, '1RO', 'C', 1),
+(4, '1RO', 'D', 1),
+(5, '1RO', 'E', 1),
+(6, '2DO', 'A', 1),
+(7, '2DO', 'B', 1),
+(8, '2DO', 'C', 1),
+(9, '2DO', 'D', 1),
+(10, '2DO', 'E', 1),
+(11, '3RO', 'A', 1),
+(12, '3RO', 'B', 1),
+(13, '3RO', 'C', 1),
+(14, '3RO', 'D', 1),
+(15, '3RO', 'E', 1),
+(16, '4TO', 'A', 1),
+(17, '4TO', 'B', 1),
+(18, '4TO', 'C', 1),
+(19, '4TO', 'D', 1),
+(20, '4TO', 'E', 1),
+(21, '5TO', 'A', 1),
+(22, '5TO', 'B', 1),
+(23, '5TO', 'C', 1),
+(24, '5TO', 'D', 1),
+(25, '5TO', 'E', 1),
+(26, '6TO', 'A', 1),
+(27, '6TO', 'B', 1),
+(28, '6TO', 'C', 1),
+(29, '6TO', 'D', 1),
+(30, '6TO', 'E', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `docente`
+--
+
+DROP TABLE IF EXISTS `docente`;
+CREATE TABLE `docente` (
+  `id_docente` int(11) NOT NULL,
+  `ci` varchar(20) NOT NULL,
+  `nombre` varchar(50) CHARACTER SET utf8 COLLATE utf8_spanish2_ci NOT NULL,
+  `paterno` varchar(45) CHARACTER SET utf8 COLLATE utf8_spanish2_ci NOT NULL,
+  `materno` varchar(45) CHARACTER SET utf8 COLLATE utf8_spanish2_ci NOT NULL,
+  `celular` varchar(12) CHARACTER SET utf8 COLLATE utf8_spanish2_ci NOT NULL,
+  `estado` tinyint(1) NOT NULL DEFAULT '1',
+  `id_user` bigint(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `docente`
+--
+
+INSERT INTO `docente` (`id_docente`, `ci`, `nombre`, `paterno`, `materno`, `celular`, `estado`, `id_user`) VALUES
+(1, '123456', 'Administrador', 'admin', 'admin', '61173339', 1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `encargado`
+--
+
+DROP TABLE IF EXISTS `encargado`;
+CREATE TABLE `encargado` (
+  `id_tutor` int(11) NOT NULL,
+  `id_rude` int(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `estudiante`
+--
+
+DROP TABLE IF EXISTS `estudiante`;
+CREATE TABLE `estudiante` (
+  `id_rude` int(20) NOT NULL,
+  `nombre` varchar(50) CHARACTER SET utf8 COLLATE utf8_spanish2_ci NOT NULL,
+  `paterno` varchar(45) CHARACTER SET utf8 COLLATE utf8_spanish2_ci NOT NULL,
+  `materno` varchar(45) CHARACTER SET utf8 COLLATE utf8_spanish2_ci DEFAULT NULL,
+  `sexo` varchar(45) CHARACTER SET utf8 COLLATE utf8_spanish2_ci NOT NULL,
+  `fecha_nac` date NOT NULL,
+  `domicilio` varchar(200) CHARACTER SET utf8 COLLATE utf8_spanish2_ci NOT NULL DEFAULT 's/dir',
+  `estado` tinyint(1) NOT NULL DEFAULT '1',
+  `id_user` bigint(4) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `faltas`
+--
+
+DROP TABLE IF EXISTS `faltas`;
+CREATE TABLE `faltas` (
+  `id_falta` int(11) NOT NULL,
+  `tipoFalta` varchar(50) CHARACTER SET utf8 COLLATE utf8_spanish2_ci DEFAULT NULL,
+  `descripcion` varchar(200) CHARACTER SET utf8 COLLATE utf8_spanish2_ci DEFAULT NULL,
+  `estado` tinyint(1) DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `faltas_cometidas`
+--
+
+DROP TABLE IF EXISTS `faltas_cometidas`;
+CREATE TABLE `faltas_cometidas` (
+  `id_fal_com` bigint(20) NOT NULL,
+  `obseracion` varchar(200) CHARACTER SET utf8 COLLATE utf8_spanish2_ci NOT NULL,
+  `contador` int(10) NOT NULL,
+  `fecha` date NOT NULL,
+  `estado` tinyint(4) NOT NULL DEFAULT '1',
+  `id_kardex` int(11) NOT NULL,
+  `id_user` bigint(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `kardes_curso`
+--
+
+DROP TABLE IF EXISTS `kardes_curso`;
+CREATE TABLE `kardes_curso` (
+  `id_kardex` int(11) NOT NULL,
+  `id_curso` int(11) NOT NULL,
+  `gestion` year(4) NOT NULL,
+  `estado` char(1) COLLATE utf8_spanish2_ci DEFAULT 'a' COMMENT 'se registrara al cambiar la gestion si esta aprobado a si esta reprobado r',
+  `id_asesor` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `kardex`
+--
+
+DROP TABLE IF EXISTS `kardex`;
+CREATE TABLE `kardex` (
+  `id_kardex` int(11) NOT NULL,
+  `reset` tinyint(1) NOT NULL,
+  `gestion` year(4) NOT NULL,
+  `id_rude` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `modelo_citacion`
+--
+
+DROP TABLE IF EXISTS `modelo_citacion`;
+CREATE TABLE `modelo_citacion` (
+  `id_citacion` int(11) NOT NULL,
+  `formato` varchar(400) CHARACTER SET utf8 COLLATE utf8_spanish2_ci NOT NULL,
+  `estado` tinyint(1) NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `pfaltas`
+--
+
+DROP TABLE IF EXISTS `pfaltas`;
+CREATE TABLE `pfaltas` (
+  `id_pfalta` int(11) NOT NULL,
+  `max_faltas` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `pfaltas`
+--
+
+INSERT INTO `pfaltas` (`id_pfalta`, `max_faltas`) VALUES
+(1, 3);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `roles`
+--
+
+DROP TABLE IF EXISTS `roles`;
+CREATE TABLE `roles` (
+  `id_rol` int(11) NOT NULL,
+  `nombre` varchar(20) CHARACTER SET utf8 COLLATE utf8_spanish2_ci DEFAULT NULL,
+  `estado` tinyint(1) DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `roles`
+--
+
+INSERT INTO `roles` (`id_rol`, `nombre`, `estado`) VALUES
+(1, 'Administrador', 1),
+(2, 'Docente', 1),
+(3, 'Responsable', 1),
+(4, 'Estudiante', 1),
+(5, 'Secretaria(o)', 1),
+(6, 'Director(a)', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tiene`
+--
+
+DROP TABLE IF EXISTS `tiene`;
+CREATE TABLE `tiene` (
+  `id_curso` int(11) NOT NULL,
+  `id_asignatura` bigint(20) NOT NULL,
+  `id_docente` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tutor`
+--
+
+DROP TABLE IF EXISTS `tutor`;
+CREATE TABLE `tutor` (
+  `id_tutor` int(11) NOT NULL,
+  `nombres` varchar(50) COLLATE utf8_spanish2_ci NOT NULL,
+  `paterno` varchar(45) COLLATE utf8_spanish2_ci NOT NULL,
+  `materno` varchar(45) COLLATE utf8_spanish2_ci DEFAULT NULL,
+  `celular` varchar(12) COLLATE utf8_spanish2_ci NOT NULL,
+  `telefono` varchar(10) COLLATE utf8_spanish2_ci DEFAULT NULL,
+  `domicilio` varchar(200) COLLATE utf8_spanish2_ci NOT NULL DEFAULT 's/dir',
+  `estado` tinyint(1) NOT NULL DEFAULT '1',
+  `id_user` bigint(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+
+--
+-- Volcado de datos para la tabla `tutor`
+--
+
+INSERT INTO `tutor` (`id_tutor`, `nombres`, `paterno`, `materno`, `celular`, `telefono`, `domicilio`, `estado`, `id_user`) VALUES
+(17, 'Luis', 'Mendoza', 'Ticona', '12345678', '12345678', 'Zona de Villa', 1, 20);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario`
+--
+
+DROP TABLE IF EXISTS `usuario`;
+CREATE TABLE `usuario` (
+  `id_usuario` int(11) NOT NULL,
+  `nombre_usuario` varchar(20) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `estado` tinyint(1) NOT NULL DEFAULT '1',
+  `id_rol` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `usuario`
+--
+
+INSERT INTO `usuario` (`id_usuario`, `nombre_usuario`, `password`, `estado`, `id_rol`) VALUES
+(1, 'admin', '$2y$10$YI9C55vOApfscCBpGVn5iObXDmCfwyj6BFuJY66n3dDtkW3ATlSVK', 1, 1),
+(20, 'L.Mendoza', '$2y$10$sWB3NeuJzpGvGiKaaETV3.1AiYto2yqVKKLhXrQetdLhgpOjlmO5O', 1, 3);
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `asignatura`
+--
+ALTER TABLE `asignatura`
+  ADD PRIMARY KEY (`id_asignatura`);
+
+--
+-- Indices de la tabla `citacion`
+--
+ALTER TABLE `citacion`
+  ADD PRIMARY KEY (`id_citacion`),
+  ADD KEY `id_kar` (`id_kardex`);
+
+--
+-- Indices de la tabla `curso`
+--
+ALTER TABLE `curso`
+  ADD PRIMARY KEY (`id_curso`);
+
+--
+-- Indices de la tabla `docente`
+--
+ALTER TABLE `docente`
+  ADD PRIMARY KEY (`id_docente`),
+  ADD UNIQUE KEY `ci_UNIQUE` (`ci`),
+  ADD UNIQUE KEY `id_user_UNIQUE` (`id_user`);
+
+--
+-- Indices de la tabla `encargado`
+--
+ALTER TABLE `encargado`
+  ADD PRIMARY KEY (`id_tutor`,`id_rude`),
+  ADD KEY `id_rude1` (`id_rude`);
+
+--
+-- Indices de la tabla `estudiante`
+--
+ALTER TABLE `estudiante`
+  ADD PRIMARY KEY (`id_rude`);
+
+--
+-- Indices de la tabla `faltas`
+--
+ALTER TABLE `faltas`
+  ADD PRIMARY KEY (`id_falta`);
+
+--
+-- Indices de la tabla `faltas_cometidas`
+--
+ALTER TABLE `faltas_cometidas`
+  ADD PRIMARY KEY (`id_fal_com`),
+  ADD KEY `id_kardex` (`id_kardex`);
+
+--
+-- Indices de la tabla `kardes_curso`
+--
+ALTER TABLE `kardes_curso`
+  ADD PRIMARY KEY (`id_kardex`,`id_curso`),
+  ADD KEY `id_curso` (`id_curso`);
+
+--
+-- Indices de la tabla `kardex`
+--
+ALTER TABLE `kardex`
+  ADD PRIMARY KEY (`id_kardex`),
+  ADD KEY `id_rude` (`id_rude`);
+
+--
+-- Indices de la tabla `modelo_citacion`
+--
+ALTER TABLE `modelo_citacion`
+  ADD PRIMARY KEY (`id_citacion`);
+
+--
+-- Indices de la tabla `pfaltas`
+--
+ALTER TABLE `pfaltas`
+  ADD PRIMARY KEY (`id_pfalta`);
+
+--
+-- Indices de la tabla `roles`
+--
+ALTER TABLE `roles`
+  ADD PRIMARY KEY (`id_rol`);
+
+--
+-- Indices de la tabla `tiene`
+--
+ALTER TABLE `tiene`
+  ADD PRIMARY KEY (`id_curso`,`id_asignatura`,`id_docente`),
+  ADD KEY `id_asignatura1` (`id_asignatura`),
+  ADD KEY `id_docente` (`id_docente`);
+
+--
+-- Indices de la tabla `tutor`
+--
+ALTER TABLE `tutor`
+  ADD PRIMARY KEY (`id_tutor`);
+
+--
+-- Indices de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`id_usuario`,`id_rol`),
+  ADD UNIQUE KEY `nombre_usuario_UNIQUE` (`nombre_usuario`),
+  ADD KEY `id_rol` (`id_rol`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `asignatura`
+--
+ALTER TABLE `asignatura`
+  MODIFY `id_asignatura` bigint(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+--
+-- AUTO_INCREMENT de la tabla `citacion`
+--
+ALTER TABLE `citacion`
+  MODIFY `id_citacion` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `curso`
+--
+ALTER TABLE `curso`
+  MODIFY `id_curso` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+--
+-- AUTO_INCREMENT de la tabla `docente`
+--
+ALTER TABLE `docente`
+  MODIFY `id_docente` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT de la tabla `estudiante`
+--
+ALTER TABLE `estudiante`
+  MODIFY `id_rude` int(20) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `faltas`
+--
+ALTER TABLE `faltas`
+  MODIFY `id_falta` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `faltas_cometidas`
+--
+ALTER TABLE `faltas_cometidas`
+  MODIFY `id_fal_com` bigint(20) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `modelo_citacion`
+--
+ALTER TABLE `modelo_citacion`
+  MODIFY `id_citacion` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `pfaltas`
+--
+ALTER TABLE `pfaltas`
+  MODIFY `id_pfalta` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT de la tabla `roles`
+--
+ALTER TABLE `roles`
+  MODIFY `id_rol` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+--
+-- AUTO_INCREMENT de la tabla `tutor`
+--
+ALTER TABLE `tutor`
+  MODIFY `id_tutor` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+--
+-- AUTO_INCREMENT de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `citacion`
+--
+ALTER TABLE `citacion`
+  ADD CONSTRAINT `id_kar` FOREIGN KEY (`id_kardex`) REFERENCES `kardex` (`id_kardex`);
+
+--
+-- Filtros para la tabla `encargado`
+--
+ALTER TABLE `encargado`
+  ADD CONSTRAINT `id_rude1` FOREIGN KEY (`id_rude`) REFERENCES `estudiante` (`id_rude`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `id_tutor1` FOREIGN KEY (`id_tutor`) REFERENCES `tutor` (`id_tutor`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `faltas_cometidas`
+--
+ALTER TABLE `faltas_cometidas`
+  ADD CONSTRAINT `id_kardex` FOREIGN KEY (`id_kardex`) REFERENCES `kardex` (`id_kardex`);
+
+--
+-- Filtros para la tabla `kardes_curso`
+--
+ALTER TABLE `kardes_curso`
+  ADD CONSTRAINT `kardes_curso_ibfk_1` FOREIGN KEY (`id_kardex`) REFERENCES `kardex` (`id_kardex`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `kardes_curso_ibfk_2` FOREIGN KEY (`id_curso`) REFERENCES `curso` (`id_curso`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `kardex`
+--
+ALTER TABLE `kardex`
+  ADD CONSTRAINT `id_rude` FOREIGN KEY (`id_rude`) REFERENCES `estudiante` (`id_rude`);
+
+--
+-- Filtros para la tabla `tiene`
+--
+ALTER TABLE `tiene`
+  ADD CONSTRAINT `id_curso2` FOREIGN KEY (`id_curso`) REFERENCES `curso` (`id_curso`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `tiene_ibfk_1` FOREIGN KEY (`id_asignatura`) REFERENCES `asignatura` (`id_asignatura`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `tiene_ibfk_2` FOREIGN KEY (`id_docente`) REFERENCES `docente` (`id_docente`);
+
+--
+-- Filtros para la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD CONSTRAINT `usuario_ibfk_1` FOREIGN KEY (`id_rol`) REFERENCES `roles` (`id_rol`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
