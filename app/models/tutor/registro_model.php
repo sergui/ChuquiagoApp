1baa239a29ae1fffba3c6bf054cc2e797eee876a
Crash Override <infcle@gmail.com>
2018-04-23 17:55:10 -0400

    modificando la insercion de datos de tutor

--- app/models/tutor/registro_model.php
+++ app/models/tutor/registro_model.php
@@ -18,23 +18,37 @@
 	$sqlSearchNomUser = "SELECT id_usuario FROM usuario WHERE nombre_usuario = '".$nombreUser."'";
 	$resSearchNomUser = $con->query($sqlSearchNomUser);
 
-	if($resSearchNomUser->num_rows == 0){			
-			$sqlInsertUser = "INSERT INTO usuario (nombre_usuario, password, estado, id_rol) VALUES ('{$nombreUser}', '{$contraseniaUser}', '1', '3')";
-			if(!$con->query($sqlInsertUser)){
-				echo ("<h3>ERROR AL INSERTAR EL USUARIO</h3>");
-			}else{
-				$sqlIdUser = "SELECT id_usuario FROM usuario ORDER BY id_usuario DESC LIMIT 1";
-				$resIdUser = $con->query($sqlIdUser);
-				if($resIdUser->num_rows == 1){
-					$fila = $resIdUser->fetch_array();
-					$sql = "INSERT INTO tutor(nombres, paterno, materno, celular, telefono, domicilio,estado, id_user) VALUES('{$nombres}', '{$paterno}', '{$materno}', '{$celular}', '{$telefono}', '{$domicilio}', 1,'{$fila[0]}')";
-						if (!$con->query($sql)) {
-							echo "Falló la insercion: (" . $con->errno . ") " . $con->error;
-						}
-						else
-							echo 1;
+	if($resSearchNomUser->num_rows == 0){
+		$sqlInsertUser = "INSERT INTO usuario (nombre_usuario, password, estado, id_rol) VALUES ('{$nombreUser}', '{$contraseniaUser}', '1', '3')";
+		if(!$con->query($sqlInsertUser)){
+			$jsondata['estado']="0";//error al registrar
+			//echo ("<h3>ERROR AL INSERTAR EL USUARIO</h3>");
+		}else{
+			$sqlIdUser = "SELECT id_usuario FROM usuario ORDER BY id_usuario DESC LIMIT 1";
+			$resIdUser = $con->query($sqlIdUser);
+			if($resIdUser->num_rows == 1){
+				$fila = $resIdUser->fetch_array();
+				$idUsuario=$fila[0];
+				$sql = "INSERT INTO tutor(nombres, paterno, materno, celular, telefono, domicilio,estado, id_user) VALUES('{$nombres}', '{$paterno}', '{$materno}', '{$celular}', '{$telefono}', '{$domicilio}', 1,'{$fila[0]}')";
+				if (!$con->query($sql)) {
+					$jsondata['estado']="0";
+					//echo "Falló la insercion: (" . $con->errno . ") " . $con->error;
 				}
-			}			
+				else{
+					$jsondata['estado']="1";
+					$sqlidTutor="SELECT id_tutor from tutor where id_user={$idUsuario}";
+					if (!($residTutor=$con->query($sqlidTutor))) {
+						$jsondata['estado']="no se encuentra el tutor";
+					}else {
+						$jsondata['estado']="1";
+						while ($row = $residTutor->fetch_array() ) {
+							$jsondata['tutor'] = $row;
+						}
+					}
+					//echo 1;
+				}
+			}
+		}
 	}else{
 		//echo ("<h3>YA EXISTE USUARIO</h3>");
 		$sqlIdUserLast = "SELECT id_usuario FROM usuario ORDER BY id_usuario DESC LIMIT 1";
@@ -46,22 +60,36 @@
 			$contraseniaUser = password_hash($nombreUser, PASSWORD_DEFAULT);
 			$sqlInsertUser = "INSERT INTO usuario (id_usuario, nombre_usuario, password, estado, id_rol) VALUES (NULL, '{$nombreUser}', '{$contraseniaUser}', '1', '3')";
 			if(!$con->query($sqlInsertUser)){
-				echo ("<h3>ERROR AL INSERTAR EL USUARIO Else".$nombreUser."</h3>");
+				$jsondata['estado']="0";
+				//echo ("<h3>ERROR AL INSERTAR EL USUARIO Else".$nombreUser."</h3>");
 			}else{
 				$sqlIdUser = "SELECT id_usuario FROM usuario ORDER BY id_usuario DESC LIMIT 1";
 				$resIdUser = $con->query($sqlIdUser);
 				if($resIdUser->num_rows == 1){
 					$fila = $resIdUser->fetch_array();
+					$idUsuario=$fila[0];
 					$sql = "INSERT INTO tutor(nombres, paterno, materno, celular, telefono, domicilio,estado, id_user) VALUES('{$nombres}', '{$paterno}', '{$materno}', '{$celular}', '{$telefono}', '{$domicilio}', 1,'{$fila[0]}')";
-						if (!$con->query($sql)) {
-							echo "Falló la insercion: (" . $con->errno . ") " . $con->error;
+					if (!$con->query($sql)) {
+						$jsondata['estado']="0";
+						//echo "Falló la insercion: (" . $con->errno . ") " . $con->error;
+					}
+					else{
+						$jsondata['estado']="1";
+						$sqlidTutor="SELECT id_tutor from tutor where id_user={$idUsuario}";
+						if (!($residTutor=$con->query($sqlidTutor))) {
+							$jsondata['estado']="no se encuentra el tutor";
+						}else {
+							$jsondata['estado']="1";
+							while ($row = $residTutor->fetch_array() ) {
+								$jsondata['tutor'] = $row;
+							}
 						}
-						else						
-							echo 1;
-				}				
+						//echo 1;
+					}
+				}
 			}
 		}
 	}
-	
+	echo json_encode($jsondata);
 	$con->close();
 ?>
