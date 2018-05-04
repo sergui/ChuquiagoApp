0271788191d3fe76c45869178112ab4898e3dd96
Elmer Coronel <infcle@gmail.com>
2018-05-03 22:12:18 -0400

    modelo para asignar asesor

---
+++ app/models/curso/asignar_asesor_model.php
@@ -0,0 +1,12 @@
+<?php
+	require_once ("../../config/db.php");
+	require_once ("../../config/conexion.php");
+	$id_cur=$_REQUEST['id_curso'];
+	$id_doc=$_REQUEST['id_docente'];
+	$sql="UPDATE kardex set id_asesor={$id_doc} where id_curso={$id_cur}";
+	if (!$con->query($sql)) {
+		echo "Falló la edicion: (" . $con->errno . ") " . $con->error;
+	}else
+		echo 1;
+	$con->close();
+?>
