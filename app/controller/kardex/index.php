d50fa4f4cdf2f8ca90a0daccaa95385b3b3e74d7
Elmer Coronel <infcle@gmail.com>
2018-05-05 18:53:45 -0400

    modificando querys deacuerdo al rol

--- app/controller/kardex/index.php
+++ app/controller/kardex/index.php
@@ -12,9 +12,16 @@
 	$contenido="kardex/index.php";
     $subTitulo="Registro de faltas cometidas";
     $id_user  = $_SESSION['id_user'];
+    $rol  = $_SESSION['id_rol'];
 	$menu_a= $menus['C_KARDEX'];
+	if($rol==1 || $rol==5 || $rol==6){
+		$sql="SELECT * FROM  docente as d LEFT JOIN tiene as t on d.id_docente = t.id_docente LEFT JOIN curso as c on c.id_curso = t.id_curso WHERE d.id_user = {$id_user}";
+	}
+	}else if($rol==2){
+		$sql="SELECT * FROM  docente as d LEFT JOIN tiene as t on d.id_docente = t.id_docente LEFT JOIN curso as c on c.id_curso = t.id_curso WHERE d.id_user = {$id_user}";
+	}
 
-	if (!($cursos = $con->query("SELECT * FROM  docente as d LEFT JOIN tiene as t on d.id_docente = t.id_docente LEFT JOIN curso as c on c.id_curso = t.id_curso WHERE d.id_user = {$id_user} "))) {
+	if (!($cursos = $con->query(""))) {
     	echo "Falló SELECT: (" . $con->errno . ") " . $con->error;
 	}
 	$con->close();
