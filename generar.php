8c021e3efa749d182fa41987fbead19d7daec41a
sergui <juansergio1981@gmail.com>
2018-05-06 17:20:06 -0400

    modificacion BD PRUEBAS

--- generar.php
+++ generar.php
@@ -1,5 +1,22 @@
 <?php 
-$ci=10001;
+
+$ci=10041;
 $contrasenia = password_hash($ci, PASSWORD_DEFAULT);
-echo $contrasenia;
+echo $contrasenia;echo "<br>";
+
+$ci=10042;
+$contrasenia = password_hash($ci, PASSWORD_DEFAULT);
+echo $contrasenia;echo "<br>";
+
+$ci=10043;
+$contrasenia = password_hash($ci, PASSWORD_DEFAULT);
+echo $contrasenia;echo "<br>";
+
+$ci=10044;
+$contrasenia = password_hash($ci, PASSWORD_DEFAULT);
+echo $contrasenia;echo "<br>";
+
+$ci=10045;
+$contrasenia = password_hash($ci, PASSWORD_DEFAULT);
+echo $contrasenia;echo "<br>";
  ?>
