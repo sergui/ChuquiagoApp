00f6ab2ab24bbe99fedafa6f3861767e45499684
Crash Override <infcle@gmail.com>
2018-05-07 14:44:11 -0400

    arreglos de kardex

--- app/controller/kardex/index.php
+++ app/controller/kardex/index.php
@@ -16,7 +16,6 @@
 	$menu_a= $menus['C_KARDEX'];
 	if($rol==1 || $rol==5 || $rol==6){
 		$sql="SELECT * FROM  docente as d LEFT JOIN tiene as t on d.id_docente = t.id_docente LEFT JOIN curso as c on c.id_curso = t.id_curso WHERE d.id_user = {$id_user}";
-	}
 	}else if($rol==2){
 		$sql="SELECT * FROM  docente as d LEFT JOIN tiene as t on d.id_docente = t.id_docente LEFT JOIN curso as c on c.id_curso = t.id_curso WHERE d.id_user = {$id_user}";
 	}
