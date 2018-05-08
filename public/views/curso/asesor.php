c545975af705a3be9e5f4f9cf0cf86da9498440a
Crash Override <infcle@gmail.com>
2018-05-07 21:26:02 -0400

    quitando arhivos inesesarios

--- public/views/curso/asesor.php
+++ public/views/curso/asesor.php
@@ -53,9 +53,6 @@
 				</div>
 			</div>
 			<?php require_once 'modal_asesor.php'; ?>
-			<?php require_once 'modal_registrar.php'; ?>
-			<?php require_once 'modal_eliminar.php'; ?>
-			<?php require_once 'modal_editar.php'; ?>
 		</section>
 	</div>
 </div>
@@ -66,72 +63,5 @@
 	
 	$( document ).ready( function () {
 		$( "#tbDocente" ).dataTable();
-		/*$( "#frmRegistrar" ).validate( {
-			debug: true,
-			rules: {
-				nombre_usuario: {
-					required: true,
-					minlength: 3,
-					remote: {
-						url: "../../models/usuario/verifica.php",
-						type: 'post',
-						data: {
-							nombre_usuario: function () {
-								return $( "#nombre_usuario" ).val();
-							}
-						}
-					}
-				},
-				ci: {
-					required: true,
-					minlength: 5,
-					remote: {
-						url: "../../models/usuario/verifica.php",
-						type: 'post',
-						data: {
-							ci: function () {
-								return $( "#ci" ).val();
-							}
-						}
-					}
-				}
-			},
-			messages: {
-				nombre_usuario: {
-					remote: "Debe elegir otro nombre de usuario.",
-				},
-				ci: {
-					remote: "El numero de C.I. ya esta registrado verifique",
-				},
-			},
-			submitHandler: function ( form ) {
-				$.ajax( {
-					url: '../../models/docente/registro_model.php',
-					type: 'post',
-					data: $( "#frmRegistrar" ).serialize(),
-					beforeSend: function () {
-						transicion( "Procesando Espere...." );
-					},
-					success: function ( response ) {
-						if ( response == 1 ) {
-							$( '#btnRegistrar' ).attr( {
-								disabled: 'true'
-							} );
-							$( '#modal_Registrar' ).modal( 'hide' );
-							transicionSalir();
-							mensajes_alerta( 'DATOS GUARDADOS EXITOSAMENTE !! ', 'success', 'GUARDAR DATOS' );
-							setTimeout( function () {
-								window.location.href = '<?php echo ROOT_CONTROLLER ?>docente/index.php';
-							}, 3000 );
-						} else {
-							transicionSalir();
-							mensajes_alerta( 'ERROR AL REGISTRAR verifique los datos!! ' + response, 'error', 'GUARDAR DATOS' );
-						}
-					}
-				} );
-			}
-		} );*/
-	
-		
 	} );
 </script>
