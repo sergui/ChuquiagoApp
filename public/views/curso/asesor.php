06a263ad01cb4b554699381ba5b0ea139f64e439
Crash Override <infcle@gmail.com>
2018-05-15 17:31:44 -0400

    solucin alterna con el estilo de la tabla

--- public/views/curso/asesor.php
+++ public/views/curso/asesor.php
@@ -26,23 +26,17 @@
 
 								<td>
 									<?php if ($lista_asesor['id_asesor']==-1): ?>
-									
-								
-       					<a href="#modal_asesor" class="btn btn-xs btn-success" onClick="curso(<?php echo $lista_asesor['id_curso']; ?>)" data-toggle="modal">
-                            <span class="fa fa-user"></span> Adicionar
-                        </a>
-    										
 
+				       					<a href="#modal_asesor" class="btn btn-xs btn-success" onClick="curso(<?php echo $lista_asesor['id_curso']; ?>)" data-toggle="modal">
+				                            <span class="fa fa-user"></span> Adicionar
+				                        </a>
 									<?php else: ?> Asignado
 									<?php endif; ?>
 								</td>
-
 								<td>
 									<?php echo $lista_asesor['nombre_completo']; ?>
 								</td>
-
 								<td>
-
 									<?php echo $lista_asesor['gestion']; ?>
 								</td>
 							</tr>
@@ -52,16 +46,43 @@
 					</table>
 				</div>
 			</div>
-			<?php require_once 'modal_asesor.php'; ?>
 		</section>
 	</div>
 </div>
+<?php require_once 'modal_asesor.php'; ?>
 <script>
+	$( document ).ready( function () {
+		$( "#tbAsesores" ).dataTable();
+		$( "#tb-Docente" ).dataTable();
+	} );
+	function registro( id ) {
+		var idc=$('#cursoid').val();
+        $.ajax( {
+            url: '../../models/curso/asignar_asesor_model.php',
+            type: 'POST',
+            data: {
+                id_curso: idc,
+                id_docente: id
+            },
+            beforeSend: function() {
+                transicion("Procesando Espere....");
+            },
+            success: function(response) {
+	            if(response==1){
+	                $('#modal_asesor').modal('hide');
+	                transicionSalir();
+	                mensajes_alerta('DATOS GUARDADOS EXITOSAMENTE !! ','success','GUARDAR DATOS');
+	                setTimeout(function(){
+	                    window.location.href='<?php echo ROOT_CONTROLLER ?>curso/asesor.php';
+	                }, 3000);
+	            }else{
+	                transicionSalir();
+	                mensajes_alerta('ERROR AL REGISTRAR verifique los datos!! '+response,'error','GUARDAR DATOS');
+	            }
+            }
+        } );
+    }
 	function curso(idcurso){
 		$('#cursoid').val(idcurso);
 	}
-	
-	$( document ).ready( function () {
-		$( "#tbDocente" ).dataTable();
-	} );
 </script>
