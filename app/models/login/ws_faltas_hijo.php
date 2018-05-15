dba037f038867f61493a489babfd80c3aa615c5c
Laiwett <l.mendoza.liet@gmail.com>
2018-05-15 16:52:43 -0400

    mas webservices

---
+++ app/models/login/ws_faltas_hijo.php
@@ -0,0 +1,36 @@
+<?php
+	require_once ("../../config/db.php");
+    require_once ("../../config/conexion.php");
+    
+    $json=array();
+    if(isset($_GET["id_rude"])){
+        $id_rude = $_GET["id_rude"];
+        $sql="SELECT k.gestion, f.tipoFalta, f.descripcion, fc.obseracion, fc.fecha
+        FROM kardex as k  
+        RIGHT JOIN faltas_cometidas fc ON fc.id_kardex=k.id_kardex
+        RIGHT JOIN faltas f ON f.id_falta = fc.id_falta
+        WHERE k.id_rude = {$id_rude}";
+              
+        if($result = $con->query($sql)){
+            if($result->num_rows > 0){
+                //$jsondata['estado']="correcto";                
+                while ($row = $result->fetch_array()) {
+                    $json['faltashijo'][]=$row;                                                    
+                }
+            }else{
+                $faltashijo['estado']="0";//No existe registros en la consulta
+                $json['faltashijo'][]=$faltashijo;                
+            }
+        }else{
+            $faltashijo['estado']="Error en la consulta";
+            $json['faltashijo'][]=$faltashijo;             
+        }
+        echo json_encode($json);
+    }else{
+        $faltashijo['estado']="1";//Error de usuario no encuentra
+        $json['faltashijo'][]=$faltashijo;
+        echo json_encode($json);
+    } 
+      
+    $con->close();
+?>
