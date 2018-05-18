f9a583f31e7c726ed278782ab75db02307135926
Laiwett <l.mendoza.liet@gmail.com>
2018-05-17 20:19:57 -0400

    mas webservices

---
+++ app/models/login/ws_citaciones.php
@@ -0,0 +1,38 @@
+<?php
+	require_once ("../../config/db.php");
+    require_once ("../../config/conexion.php");
+    
+    $json=array();
+    if(isset($_GET["id_tutor"]) && isset($_GET["fecha"])){
+        $id_tutor = $_GET["id_tutor"];
+        $fecha = $_GET["fecha"];
+        $sql="SELECT CONCAT(es.nombre, es.paterno ,es.materno)as nombre_completo, c.citacion, c.fecha , k.id_curso, TIME(c.fecha) as hora
+        FROM encargado e
+        RIGHT JOIN estudiante es ON e.id_rude = es.id_rude
+        RIGHT JOIN kardex k ON k.id_rude = es.id_rude
+        RIGHT JOIN citacion c ON c.id_kardex=k.id_kardex AND DATE(c.fecha)='{$fecha}'        
+        WHERE e.id_tutor = {$id_tutor}";
+        //echo $sql;
+        if($result = $con->query($sql)){
+            if($result->num_rows > 0){
+                //$jsondata['estado']="correcto";                
+                while ($row = $result->fetch_array()) {
+                    $json['citacion'][]=$row;                                                    
+                }
+            }else{
+                $citacion['estado']="0";//No existe registros en la consulta
+                $json['citacion'][]=$citacion;                
+            }
+        }else{
+            $citacion['estado']="Error en la consulta";
+            $json['citacion'][]=$citacion;             
+        }
+        echo json_encode($json);
+    }else{
+        $citacion['estado']="1";//Error de usuario no encuentra
+        $json['citacion'][]=$citacion;
+        echo json_encode($json);
+    } 
+      
+    $con->close();
+?>
--- app/models/login/ws_faltas_hijo.php
+++ app/models/login/ws_faltas_hijo.php
@@ -3,11 +3,12 @@
     require_once ("../../config/conexion.php");
     
     $json=array();
-    if(isset($_GET["id_rude"])){
+    if(isset($_GET["id_rude"])&& isset($_GET["fecha"])){
         $id_rude = $_GET["id_rude"];
+        $fecha = $_GET["fecha"];
         $sql="SELECT k.gestion, f.tipoFalta, f.descripcion, fc.obseracion, fc.fecha, a.nombre_asignatura
         FROM kardex as k  
-        RIGHT JOIN faltas_cometidas fc ON fc.id_kardex=k.id_kardex
+        RIGHT JOIN faltas_cometidas fc ON fc.id_kardex=k.id_kardex AND DATE(fc.fecha)='{$fecha}'
         RIGHT JOIN faltas f ON f.id_falta = fc.id_falta
         RIGHT JOIN asignatura a ON a.id_asignatura = fc.id_asignatura
         WHERE k.id_rude = {$id_rude}";
@@ -19,12 +20,14 @@
                     $json['faltashijo'][]=$row;                                                    
                 }
             }else{
-                $faltashijo['estado']="0";//No existe registros en la consulta
+                $faltashijo="";//No existe registros en la consulta
                 $json['faltashijo'][]=$faltashijo;                
+                               
             }
         }else{
-            $faltashijo['estado']="Error en la consulta";
-            $json['faltashijo'][]=$faltashijo;             
+            $faltashijo="";
+            $json['faltashijo'][]=$faltashijo; 
+                       
         }
         echo json_encode($json);
     }else{
