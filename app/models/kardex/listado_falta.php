af8ffc325d94d6af85c433d679e5de60d62b194f
Crash Override <infcle@gmail.com>
2018-05-18 15:10:14 -0400

    cambios en registro

--- app/models/kardex/listado_falta.php
+++ app/models/kardex/listado_falta.php
@@ -12,13 +12,37 @@
     <div class="form-group">
         <label for="newsletter" class="control-label col-md-10"><?php echo $falta['descripcion']; ?></label>
         <div class="col-md-2">
-            <input type="checkbox" style="width: 20px" class="checkbox form-control" id="<?php echo $falta['id_falta']; ?>" name="id_falta[]" value="<?php echo $falta['id_falta']; ?>" required minlength='1'/>
+            <input type="checkbox" style="width: 20px" class="checkbox form-control micheckbox" id="<?php echo $falta['id_falta']; ?>" name="id_falta[]" value="<?php echo $falta['id_falta']; ?>" required minlength='1'/>
+        </div>
+    </div>
+    <div class="form-group hide" id="o_<?php echo $falta['id_falta']; ?>">
+        <label class="col-sm-3 control-label">OBSERVACION</label>
+        <div class="col-sm-9">
+            <textarea rows="5" disabled="true" class="form-control" id="observacion_<?php echo $falta['id_falta']; ?>" name="observacion[]"></textarea>
         </div>
     </div>
 <?php endforeach ?>
-<div class="form-group">
+<!-- <div class="form-group">
     <label class="col-sm-3 control-label">OBSERVACION</label>
     <div class="col-sm-9">
-        <textarea rows="5" class="form-control" id="observacion" name="observacion"></textarea>
+        <textarea rows="5" class="form-control" id="observacion[]" name="observacion[]"></textarea>
     </div>
-</div>
+</div> -->
+<script>
+    $( '.micheckbox' ).on( 'click', function() {
+    if( $(this).is(':checked') ){
+        $('#observacion_'+$(this).val()).val('');
+        $('#o_'+$(this).val()).removeClass('hide');
+        $('#observacion_'+$(this).val()).removeAttr('disabled');
+        $('#observacion_'+$(this).val()).focus();
+        // Hacer algo si el checkbox ha sido seleccionado
+        //alert("El checkbox con valor " + $(this).val() + " ha sido seleccionado");
+    } else {
+        $('#o_'+$(this).val()).addClass('hide');
+        $('#observacion_'+$(this).val()).attr('disabled', 'true');
+        // Hacer algo si el checkbox ha sido deseleccionado
+        //alert("El checkbox con valor " + $(this).val() + " ha sido deseleccionado");
+    }
+});
+
+</script>
--- app/models/kardex/registro_model.php
+++ app/models/kardex/registro_model.php
@@ -5,7 +5,7 @@
 	//echo "<pre>";print_r ($_REQUEST);echo "</pre>";
 	session_start();
 
-    $observacion = trim($_POST["observacion"]);
+    
     $contador    = 0;
     $fecha       = "";
     $id_kardex   = trim($_POST["id_kardex"]);
@@ -15,6 +15,7 @@
 	$verifica=0;
 	for ($i=0; $i <$cantidad ; $i++) {
 		$id_falta=$_REQUEST['id_falta'][$i];
+		$observacion = trim($_POST["observacion"][$i]);
 		$sql = "INSERT INTO faltas_cometidas (obseracion, contador, fecha, id_kardex, id_user ,id_falta, id_asignatura) VALUES ('{$observacion}',{$contador},CURDATE(),{$id_kardex},{$id_user},{$id_falta}, {$id_asignatura});";
 		if (!$con->query($sql)) {
 			echo "Falló la insercion falta cometidas: (" . $con->errno . ") " . $con->error;
