a91d8f141250f3ec0c607c5aa666a7ad9f9f4585
Crash Override <infcle@gmail.com>
2018-05-18 14:25:49 -0400

    reemplaando el index

---
+++ app/controller/falta/index.php
@@ -0,0 +1,27 @@
+
+<?php
+	require_once '../../config/route.php';
+	session_start();
+	if (!isset($_SESSION['user_login_status']) AND $_SESSION['user_login_status'] != 1) {
+        header("location: ".ROOT_CONTROLLER.'login/');
+		exit;
+	}
+	require_once ("../../config/db.php");
+	require_once ("../../config/conexion.php");
+	//Variables para enviar a la plantilla
+	$titulo="Faltas";
+	$contenido="falta/index.php";
+	$menu_a= $menus['C_FALTA'];
+	$subTitulo="FALTAS";
+	
+	if (!($faltas = $con->query("SELECT id_falta,
+									tipoFalta tipoFalta
+									,descripcion
+									FROM faltas
+									WHERE estado=1"))) {
+    	echo "Falló SELECT: (" . $con->errno . ") " . $con->error;
+	}
+	$con->close();
+	//$pie_class="si";
+	require_once ('../../../public/views/plantilla.php');
+?>
